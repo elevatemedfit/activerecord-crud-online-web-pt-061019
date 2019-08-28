327f7d965a8bfd9a884e00196af6c0bde998fb86
Chris Junker <elevatemedfit@gmail.com>
2019-08-28 17:05:00 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -83,7 +83,6 @@ def can_update_multiple_items_at_once # ????
   # Change title of all movies to "A Movie"
   5.times do |i|
     Movie.create(title: "Movie_#{i}", release_date: 2000+i)
-    binding.pry
   end
   Movie.update_all(title: "A Movie")
 end
